12a464589b014ab5fa5b6282d7c93e1cd115ad34
Mauri Persico <mauripersico@gmail.com>
2025-03-15 15:33:25 -0300

    segunda entrega js

--- main.js
+++ main.js
@@ -1,49 +1,105 @@
-const partidos = ['Boca', 'River', 'Gimnsia'];
-const ubicaciones = ['Popular', 'Platea', 'Palco'];
-let sinEntradas = true;
+let eventos = [];
 
-function comprarEntradas(){
-    do {
-        // Mostrar opciones de partidos
-        const partidosIndex = prompt(`
-            ¿Entradas para el partido de?
-            1 - Boca
-            2 - River
-            3 - Gimnasia
-        `);
-            
-        // Validar el partido
-        const partido = partidos [partidosIndex - 1];
-        if (!partido) {
-            alert("Opción inválida. Intente de nuevo.");
-            continue; // Volver a empezar
-        }
+let arreglos = []; // Carga de información
 
-        // Mostrar opciones de ubicación
-        const ubicacionIndex = prompt(`
-            ¿Qué ubicación querés?
-            1 - Popular
-            2 - Platea
-            3 - Palco
-        `);
+const eventoNombre = document.querySelector('#eventoNombre');
+const eventoFecha = document.querySelector('#eventoFecha');
+const botonAdd = document.querySelector('#bAdd');
+const eventosConteiner = document.querySelector('#eventosConteiner');
 
-        // Validar la ubicación
-        const ubicacion = ubicaciones[ubicacionIndex - 1];
-        if (!ubicacion) {
-            alert("Opción inválida. Intente de nuevo.");
-            continue;
-        }
+const json = cargar();
 
-        // Confirmar compra
-        const confirmaEntrada = confirm(`¿Confirma que va a comprar una entrada para el partido de ${partido} y la ubicación ${ubicacion}?`);
-        if (confirmaEntrada) {
-            alert("Compra realizada con éxito");
-            sinEntradas = false;
-        } else {
-            alert("Compra cancelada");
-        }
+try {
+    arreglos = JSON.parse(json);
+    } catch (error){ 
+        arreglos = [];   
+    }
+    eventos = arreglos ? [...arreglos]: [];
 
-    } while (sinEntradas);
+    renderizarEventos();
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  addEvent();
+})
+
+botonAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+    addEvent();
+  })
+
+function addEvent(){
+    if (eventoNombre.value == "" || eventoFecha.value == ""){
+        return;
+    }
+
+    if (fechaDiferencia(eventoFecha.value) < 0){
+        return;
+    }
+
+
+    const  nuevoEvento = {
+        id: (Math.random() * 100). toString(36).slice(3),
+        nombre: eventoNombre.value,
+        fecha: eventoFecha.value,
+    };
+
+    eventos.unshift(nuevoEvento);
+
+    guardar(JSON.stringify(eventos));
+
+
+    eventoNombre.value = ""; 
+
+    renderizarEventos();
+
 }
 
-comprarEntradas();
+function fechaDiferencia(d){
+    const fechaObjetivo = new Date(d)
+    const hoy = new Date()
+    const diferencia = fechaObjetivo.getTime() - hoy.getTime();
+    const días = Math.ceil(diferencia / (1000 * 3600 * 24));
+    return días;
+
+} 
+
+function renderizarEventos(){
+    const eventosHTML = eventos.map(evento =>{
+        return ` 
+        <div class ="evento">
+            <div class = "días" >
+                <span class= "días-numeros">${fechaDiferencia(evento.fecha)}</span>
+                <span class="días-texto ">días</span>
+
+            </div>
+            <div class="evento-nombre">${evento.nombre}</div>
+            <div class="evento-fecha">${evento.fecha}</div>
+            <div class="acciones" >
+                <button class= "bBorrar" data-id="${evento .id}">Eliminar</button>
+            </div>
+        </div>
+        `;
+
+      
+    })
+    eventosConteiner.innerHTML = eventosHTML.join("");
+    document.querySelectorAll('.bBorrar').forEach(button =>{
+        button.addEventListener('click', e =>{
+            const id = button.getAttribute('data-id');
+            eventos = eventos.filter(evento => evento.id !== id);
+
+            renderizarEventos();
+        })
+    })
+}
+
+
+function guardar(data){
+    localStorage.setItem('items',data);
+
+}
+
+function cargar(){
+    return localStorage.getItem("items");
+}
