63b3a8c00287574dd4dd7862030a0dd79f708b32
Joaquin Barrionuevo <mauripersico@gmail.com>
2025-05-02 21:41:13 -0300

    Primer Entrega React

--- react 1/pablo-CreaTuLanding1-Persico/src/components/CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-const CartWidget = () => {
-    return (
-      <div className="cart-widget">
-        🛒 <span style={{ marginLeft: "5px" }}>Carrito</span>
-      </div>
-    );
-  };
-
-  export default CartWidget;
